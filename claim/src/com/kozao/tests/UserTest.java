01fe889fe4bb2532c8ddca89d35906afcd94bfa8
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2025-08-25 19:11:55 +0100

    Modification of the UserTest class

--- claim/src/com/kozao/tests/UserTest.java
+++ claim/src/com/kozao/tests/UserTest.java
@@ -1,10 +1,34 @@
 package com.kozao.tests;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.kozao.controllers.UserController;
+
+
 public class UserTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
 
+	UserController userController;
+
+	@BeforeEach
+	void setUp() {
+		userController = new UserController();
 	}
 
+	@Test
+    void testCreateAndRead() {
+		
+		userController.addUserController("DJIOLEU", "Wilfried", "djioleuwilfried@gmail.com", "admin");
+        
+        // assertNotNull(result);
+        assertEquals(UserController.msgUserController, "kjgfh");
+    }
+	
+	
+	
 }
