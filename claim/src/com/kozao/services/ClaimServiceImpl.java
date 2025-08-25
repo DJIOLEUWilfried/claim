cdd7a426fe1508d2cb6b8a8ad5c12af57debd6da
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2025-08-25 15:08:02 +0100

    Creation of the ClaimServiceImpl class

---
+++ claim/src/com/kozao/services/ClaimServiceImpl.java
@@ -0,0 +1,47 @@
+package com.kozao.services;
+
+import java.util.List;
+
+import com.kozao.models.Claim;
+import com.kozao.models.Resource;
+import com.kozao.models.User;
+
+public class ClaimServiceImpl implements ClaimService{
+
+	@Override
+	public int addReclamation(Claim claim, User user, Resource resource) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateReclamation(Claim claim, User user, Resource resource) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteReclamation(int claimId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Claim findReclamationById(int claimId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Claim findReclamationByIdUser(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Claim> findAllReclamation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
