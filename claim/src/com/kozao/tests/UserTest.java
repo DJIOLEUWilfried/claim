83394a71858bdb14c15c3c4ca18bda12addcb292
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2025-08-26 20:51:12 +0100

    Implementation of the testdeleteUserController() method of the UserTest class

--- claim/src/com/kozao/tests/UserTest.java
+++ claim/src/com/kozao/tests/UserTest.java
@@ -14,65 +14,51 @@ public class UserTest {
 
 	public UserController userController = new UserController();
 	
+	/*
+	@Test
+	public void testCreateUser() {
+
+		userController.addUserController("DJIOLEU1", "Wilfried1", "djioleuwilfried@gmail.com", "User");
+
+		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_CREATE_USER,
+				UserController.msgUserController);
+	}
 	
-//	@Test
-//	public void testCreateUser() {
-//
-//		userController.addUserController("DJIOLEU1", "Wilfried1", "djioleuwilfried@gmail.com", "User");
-//
-//		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_CREATE_USER,
-//				UserController.msgUserController);
-//	}
-//	
+	@Test
+	public void testupdateUserProfilController() {
+
+		userController.updateUserProfilController("DJAMEN1", "Jores1", "djamenjores1@gmail.com", 13);
+
+		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_UPDATE_PROFIL,
+				UserController.msgUserController);
+	}	
 	
-//	@Test
-//	public void testupdateUserProfilController() {
-//
-//		userController.updateUserProfilController("DJAMEN1", "Jores1", "djamenjores1@gmail.com", 13);
-//
-//		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_UPDATE_PROFIL,
-//				UserController.msgUserController);
-//	}
-//	
-	
-	
-//	@Test  
-//	public void testUpdatePassWordController() {
-//
-//		userController.updatePassWordController("DJIOLEU1", "ras2", "ras3");
-//		
-//		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_UPDATE_PASSWORD,
-//				UserController.msgUserController);
-//	}
-	
-	
-//	@Test  
-//	public void testdisableUserController() {
-//
-//		userController.disableUserController(18);
-//		
-//		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_DISABLE_USER_STATUS,
-//				UserController.msgUserController);
-//	}
-	
-	
-//	@Test  
-//	public void testenableUserController() {
-//
-//		userController.enableUserController(18);
-//		
-//		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_ENABLE_USER_STATUS,
-//				UserController.msgUserController);
-//	}	
-	
-//	@Test  
-//	public void testloginController() { // "L'objet ne doit pas être null"
-//
-//		User user = userController.loginController("djioleuwilfried@gmail.com", "Claim25");
-//		
-//		assertNotNull(ClaimConstanteUtil.MSG_USER_NOT_NULL, user);
-//	}	
-	
+	@Test  
+	public void testUpdatePassWordController() {
+
+		userController.updatePassWordController("DJIOLEU1", "ras2", "ras3");
+		
+		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_UPDATE_PASSWORD,
+				UserController.msgUserController);
+	}
+		
+	@Test  
+	public void testdisableUserController() {
+
+		userController.disableUserController(18);
+		
+		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_DISABLE_USER_STATUS,
+				UserController.msgUserController);
+	}
+		
+	@Test  
+	public void testenableUserController() {
+
+		userController.enableUserController(18);
+		
+		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_ENABLE_USER_STATUS,
+				UserController.msgUserController);
+	}	
 	
 	@Test  
 	public void testfindUserByIdController() { 
@@ -106,16 +92,23 @@ public class UserTest {
 		
 		assertNotNull(ClaimConstanteUtil.MSG_USER_NOT_NULL, user);
 	}
-	
-
+	*/
 	@Test  
 	public void testdeleteUserController() {
 
-		userController.deleteUserController(1);
+		userController.deleteUserController(16);
 		
 		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.QUERY_USER_DELETE,
 				UserController.msgUserController);
 	}
+	
+//	@Test  
+//	public void testloginController() { // "L'objet ne doit pas être null"
+//
+//		User user = userController.loginController("djioleuwilfried@gmail.com", "Claim25");
+//		
+//		assertNotNull(ClaimConstanteUtil.MSG_USER_NOT_NULL, user);
+//	}
 	
 	
 	
