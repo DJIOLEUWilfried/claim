de92dc423de176fb9a5b1a00bb445229aab8184e
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2025-08-26 09:55:16 +0100

    Modification of the testCreateUser() method of the UserTest class

--- claim/src/com/kozao/tests/UserTest.java
+++ claim/src/com/kozao/tests/UserTest.java
@@ -1,28 +1,50 @@
 package com.kozao.tests;
 
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.BeforeEach;
+
+//import org.junit.jupiter.api.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
 import com.kozao.controllers.UserController;
+import com.kozao.models.User;
+import com.kozao.utils.ClaimConstanteUtil;
 
 
 public class UserTest {
 
-	public UserController userController;
+	public UserController userController = new UserController();
 
-	@BeforeEach
-	void setUp() {
-		userController = new UserController();
-	}
+//	@BeforeEach
+//	void setUp() {
+//		userController = new UserController();
+//	}
 
 	@Test
-    void testCreateAndRead() {
+    public void testCreateUser() {
 		
-		userController.addUserController("DJIOLEU", "Wilfried", "djioleuwilfried@gmail.com", "admin");
-        
-        // assertNotNull(result);
-        assertEquals(UserController.msgUserController, "kjgfh");
-    }
+		userController.addUserController("Kamga18", "Souleman", "djioleuwilfried@gmail.com", "Admin");
+                
+        // System.out.println("\n Avant : " + UserController.msgUserController);
+		
+        assertEquals("Test de création d'un utilisateur echoué", ClaimConstanteUtil.MSG_CREATE_USER, UserController.msgUserController);
+
+
+	}
+//	
+//	@Test
+//    public void testFindser() {
+//		
+//		User user = userController.findUserByIdController(1);        
+//		
+//        assertEquals("Test11 Echouer", ClaimConstanteUtil.MSG_CREATE_USER, UserController.msgUserController);
+//        
+//        
+//	}
 	
 	
 	
