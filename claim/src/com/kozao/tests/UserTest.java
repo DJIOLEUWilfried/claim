bfb3a400e28e1b15acf76c6030f75c3504ed3c2e
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2025-08-26 11:47:38 +0100

    Modification of the testCreateUser() method of the UserTest class

--- claim/src/com/kozao/tests/UserTest.java
+++ claim/src/com/kozao/tests/UserTest.java
@@ -19,33 +19,14 @@ public class UserTest {
 
 	public UserController userController = new UserController();
 
-//	@BeforeEach
-//	void setUp() {
-//		userController = new UserController();
-//	}
-
 	@Test
-    public void testCreateUser() {
+    public void testCreateUser() {  
 		
-		userController.addUserController("Kamga18", "Souleman", "djioleuwilfried@gmail.com", "Admin");
-                
-        // System.out.println("\n Avant : " + UserController.msgUserController);
-		
-        assertEquals("Test de création d'un utilisateur echoué", ClaimConstanteUtil.MSG_CREATE_USER, UserController.msgUserController);
-
-
+		userController.addUserController("DJIOLEU", "Wilfried", "djioleuwilfried@gmail.com", "Admin");
+                		
+        assertEquals("Test echoué", ClaimConstanteUtil.MSG_CREATE_USER, UserController.msgUserController);
 	}
-//	
-//	@Test
-//    public void testFindser() {
-//		
-//		User user = userController.findUserByIdController(1);        
-//		
-//        assertEquals("Test11 Echouer", ClaimConstanteUtil.MSG_CREATE_USER, UserController.msgUserController);
-//        
-//        
-//	}
-	
+
 	
 	
 }
