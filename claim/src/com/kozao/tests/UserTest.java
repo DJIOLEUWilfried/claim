498cec10ae26636dd7356bf35ad5aa96373dd958
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2025-08-26 15:26:48 +0100

    Modification of the updatePassWordController() method of the UserTest class

--- claim/src/com/kozao/tests/UserTest.java
+++ claim/src/com/kozao/tests/UserTest.java
@@ -1,39 +1,42 @@
 package com.kozao.tests;
 
-
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 import com.kozao.controllers.UserController;
 import com.kozao.utils.ClaimConstanteUtil;
 
-
 public class UserTest {
 
 	public UserController userController = new UserController();
-
-	@Test
-    public void testCreateUser() {  
-		
-		userController.addUserController("KAMGA22", "Souleman", "kamga22souleman@gmail.com", "Admin");
-                		
-        assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_CREATE_USER, UserController.msgUserController);
-	}
+   
 	
+	@Test
+	public void testCreateUser() {
+
+		userController.addUserController("KAMGA12", "Souleman", "kamga12souleman@gmail.com", "User");
+
+		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_CREATE_USER,
+				UserController.msgUserController);
+	}
+
 	@Test
 	public void testupdateUserProfilController() {
-		
+
 		userController.updateUserProfilController("DJAMEN", "Jores", "djamenjores@gmail.com", 13);
-        
-        assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_UPDATE_PROFIL, UserController.msgUserController);
+
+		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_UPDATE_PROFIL,
+				UserController.msgUserController);
 	}
 
-	@Test
-	public void updatePassWord(String oldPassword, String newPassword) {
-		// Claim545
+	
+	@Test  
+	public void updatePassWordController() {
 
-		
-		
+		userController.updatePassWordController("Claim545", "ras1");
+
+		assertEquals(ClaimConstanteUtil.MSG_TEST_FAILED, ClaimConstanteUtil.MSG_UPDATE_PASSWORD,
+				UserController.msgUserController);
 	}
 	
 	
@@ -45,4 +48,5 @@ public class UserTest {
 	
 	
 	
+
 }
