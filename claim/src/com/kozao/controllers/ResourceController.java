823c5e31bc3d50f35c132f9579d41c5f7182b60c
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2025-08-27 10:10:21 +0100

    Implementation of the addResourceController() method of the ResourceController class

--- claim/src/com/kozao/controllers/ResourceController.java
+++ claim/src/com/kozao/controllers/ResourceController.java
@@ -1,12 +1,44 @@
 package com.kozao.controllers;
 
+import com.kozao.models.Resource;
 import com.kozao.services.ResourceService;
 import com.kozao.services.ResourceServiceImpl;
+import com.kozao.utils.ClaimConstanteUtil;
+import com.kozao.utils.ClaimControlUserUtil;
+import com.kozao.utils.ClaimSendPasswordUtil;
 
 public class ResourceController {
 
 	ResourceService resourceService = new ResourceServiceImpl();	
-	public void addResourceController() {
+	Resource resource = new Resource();
+	public static String msgResourceController;
+	
+	
+	public void addResourceController(String resourceName, String resourceDescription) {
+		
+		resourceName = resourceName.trim();
+		resourceDescription = resourceDescription.trim();
+		
+		if (ClaimControlUserUtil.checkAllFields(resourceName, resourceDescription) ) {
+
+			msgResourceController = ClaimConstanteUtil.MSG_REQUIRED_FIELDS;	
+			return;
+		}  
+
+		resource.setResourceName(resourceName);
+		resource.setResourceDescription(resourceDescription);
+
+		int r = resourceService.addResource(resource);
+		
+		msgResourceController = (r > 0) ? ClaimConstanteUtil.MSG_CREATE_RESOURCE : ClaimConstanteUtil.MSG_FAILED_CREATE_RESOURCE;		
 		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
