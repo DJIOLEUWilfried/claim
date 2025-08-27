9bf4944d9213a0a13440c562e38e74ba75e409ba
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2025-08-27 18:17:43 +0100

    Creating the RoleServiceImpl class

---
+++ claim/src/com/kozao/services/RoleServiceImpl.java
@@ -0,0 +1,45 @@
+package com.kozao.services;
+
+import java.util.List;
+
+import com.kozao.models.Role;
+
+public class RoleServiceImpl implements RoleService{
+
+	@Override
+	public int addRole(Role role) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateRole(Role role) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteRole(int roleId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Role findRoleById(int roleId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Role findRoleByName(String roleName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Role> findAllRole() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
