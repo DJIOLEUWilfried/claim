cad93404e2dab7e49b80be123b65baff950e99a9
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2025-08-27 09:46:58 +0100

    Creating the ResourceController class

---
+++ claim/src/com/kozao/controllers/ResourceController.java
@@ -0,0 +1,12 @@
+package com.kozao.controllers;
+
+import com.kozao.services.ResourceService;
+import com.kozao.services.ResourceServiceImpl;
+
+public class ResourceController {
+
+	ResourceService resourceService = new ResourceServiceImpl();	
+	public void addResourceController() {
+		
+	}
+}
