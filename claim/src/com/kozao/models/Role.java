e4f04493481bdf24a1385e89c3820e97019e191b
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2025-08-28 10:53:52 +0100

    Modification of Role Class Methods

--- claim/src/com/kozao/models/Role.java
+++ claim/src/com/kozao/models/Role.java
@@ -2,4 +2,43 @@ package com.kozao.models;
 
 public class Role {
 
+	private int roleId;
+	private String roleName;
+	private String roleDescription;
+	
+	public Role() { }
+	
+	public Role(String roleName, String roleDescription) {
+		super();
+		this.roleName = roleName;
+		this.roleDescription = roleDescription;
+	}
+	
+	public Role(int roleId, String roleName, String roleDescription) {
+		super();
+		this.roleId = roleId;
+		this.roleName = roleName;
+		this.roleDescription = roleDescription;
+	}
+	
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	public String getRoleDescription() {
+		return roleDescription;
+	}
+	public void setRoleDescription(String roleDescription) {
+		this.roleDescription = roleDescription;
+	}
+	
+	
 }
